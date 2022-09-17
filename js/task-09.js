1a86c56cc90505ce0b7e0465ac63402887ec4314
NilKad <kadulin@gmail.com>
2022-09-18 02:32:17 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
+const btnRef = document.querySelector(".widget button");
+const colorValue = document.querySelector(".widget .color");
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const onButton = () => {
+  const newColor = getRandomHexColor();
+  colorValue.textContent = newColor;
+  document.body.style.backgroundColor = newColor;
+};
+
+btnRef.addEventListener("click", onButton);
