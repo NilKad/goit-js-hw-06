967a38b995165de6f352e0ad30f085badf9f351c
NilKad <kadulin@gmail.com>
2022-09-18 02:20:02 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const form = document.querySelector(".login-form");
 
+const sendForm = (event) => {
+  event.preventDefault();
+  const formElements = event.currentTarget.elements;
+  if (formElements.email.value === "" || formElements.password.value === "") {
+    alert("Все поля должны быть заполнены");
+  } else {
+    const email = formElements.email.value;
+    const password = formElements.password.value;
+
+    const formData = {
+      email,
+      password,
+    };
+    console.log(formData);
+    form.reset();
+  }
+};
+
+form.addEventListener("submit", sendForm);
